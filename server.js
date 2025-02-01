f7a884bc94b01af06160dde1f4bf4c56b1b0dca4
Joe Heumer <joe.heumer@desy.de>
2025-02-01 10:40:46 +0100

    Add server configuration, middleware and API routes; improve logging and documentation

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+//==============================================================================================================
+// Server Setup
+// This file sets up the Express server, applies middleware, and defines API routes.
+// It initializes the API endpoints and starts the server on a specified port.
+//==============================================================================================================
+
+
 require('dotenv').config();  // Lädt .env-Inhalt in process.env
 const express = require('express');
 const helmet = require('helmet');
@@ -5,23 +12,45 @@ const cors = require('cors');
 const itemsRoutes = require('./routes/items');
 const authRoutes = require('./routes/auth');
 const app = express();
+const log = require('./src/log');
 
-// Middleware
+//==============================================================================================================
+// Changelog
+//==============================================================================================================
+//
+// Version 0.2: February 1, 2025
+// * Added middleware for logging
+//
+// Version 0.1: January 31, 2025
+// * Initial implementation
+//
+//==============================================================================================================
+
+
+//--------------------------------------------------------------------------------------------------------------
+// Middleware Configuration
+//--------------------------------------------------------------------------------------------------------------
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 
-// Standard-Endpoint zum Test
+app.set('trust proxy', true);
+
+//--------------------------------------------------------------------------------------------------------------
+// API Endpoints
+//--------------------------------------------------------------------------------------------------------------
 app.get('/', (req, res) => {
   res.send('API läuft!');
 });
 
-// Routen für Items
 app.use('/api/items', itemsRoutes);
 app.use('/api/auth', authRoutes);
 
-// Serverstart
+//--------------------------------------------------------------------------------------------------------------
+// Start Server
+//--------------------------------------------------------------------------------------------------------------
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server läuft auf Port ${PORT}`);
+  console.log(`Server runs on Port ${PORT}`);
+  log.writeLog(`Server runs on Port ${PORT}`, 'server', 'app.listen');
 });
